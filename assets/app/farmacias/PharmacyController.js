d1d8d5cba833485a20a33c43de8dff475cd25e3e
NicoCRoce <ingcrocenicolas@outlook.com>
2017-03-06 00:51:30 -0300

    FARMACIAS Se agregó el mapa

--- assets/app/farmacias/PharmacyController.js
+++ assets/app/farmacias/PharmacyController.js
@@ -4,9 +4,9 @@
         .module('servicios-chabas')
         .controller('PharmacyController', PharmacyController);
 
-    PharmacyController.$inject = ['$scope', 'factoryFarmacy', '$q'];
+    PharmacyController.$inject = ['$scope', 'factoryFarmacy', '$rootScope'];
 
-    function PharmacyController($scope, factoryFarmacy) {
+    function PharmacyController($scope, factoryFarmacy, $rootScope) {
 
         $scope.dataPharmacy;
 
@@ -21,8 +21,9 @@
                 img: dataResponse.imagen,
                 address: dataResponse.direccion,
                 phone: dataResponse.telefono,
-                map: dataResponse.mapa
+                map: dataResponse.mapa,
             };
+            initMap();
         };
 
         function calendarError(dataError) {
@@ -37,6 +38,29 @@
             if (angular.isUndefinedOrNullOrEmpty(row) || !row.hasDetail) return;
             $scope.modal.showModal = true;
             $scope.modal.detail = row.detail;
+        }
+
+        function initMap() {
+            var myLatLng = $scope.dataPharmacy.map;
+            var map = new google.maps.Map(document.getElementById('map'), {
+                zoom: 16,
+                center: myLatLng,
+                minZoom: 8,
+                draggable: true,
+                disableDefaultUI: true,
+                MarkerLabel: 'NICO'
+            });
+
+            new google.maps.Marker({
+                position: myLatLng,
+                map: map,
+                title: '',
+                animation: google.maps.Animation.DROP,
+                labelContent: "ABCD",
+                labelAnchor: new google.maps.Point(15, 65),
+                labelClass: "labels", // the CSS class for the label
+                labelInBackground: false,
+            });
         }
     }
 })();
