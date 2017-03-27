2d597564f57a55ba23f5d73a8b22c0759bfc6fa5
NicoCRoce <ingcrocenicolas@outlook.com>
2017-03-27 01:57:37 -0300

    Se aplicó Google Analitics.

--- assets/app/home/homeController.js
+++ assets/app/home/homeController.js
@@ -3,5 +3,13 @@
     angular
         .module('servicios-chabas')
         .controller('HomeController', ['$scope', function($scope) {
+            $scope.sendInfo = function() {
+                ga('send', {
+                    hitType: 'event',
+                    eventCategory: 'Button',
+                    eventAction: 'click',
+                    eventLabel: 'Home'
+                });
+            }
         }]);
 })();
