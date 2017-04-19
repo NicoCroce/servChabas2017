d978bce928fdbddde5b4ef9e9db7b512bce9b86d
NicoCRoce <ingcrocenicolas@outlook.com>
2017-04-19 00:05:37 -0300

    TAG e Icono - Se tageó todo. - Se creó icono.

--- assets/app/components/analytics.js
+++ assets/app/components/analytics.js
@@ -1,28 +1,34 @@
-(function(){
+(function () {
     'use strict'
     angular
         .module('servicios-chabas')
         .factory('analytics', analytics);
 
-    function analytics (){
+    function analytics() {
+
+        var path = '';
+
         return {
             pageview: pageview
         }
 
         function pageview(currentPath) {
-            return dataLayer.push({
-                'event': 'virtualPageView',
-                'dataView': {
-                    'IDanalytics': 'UA-96039991-1',
-                    'path': currentPath
-                }
-            });
+            if (path != currentPath) {
+                path = currentPath;
+                return dataLayer.push({
+                    'event': 'virtualPageView',
+                    'dataView': {
+                        'IDanalytics': 'UA-96039991-1',
+                        'path': currentPath
+                    }
+                });
+            }
 
-           /* ga(function (tracker) {
-                // Sets the page field to "/about.html".
-                tracker.set('page', currentPath);
-                tracker.send('pageview');
-            });*/
+            /* ga(function (tracker) {
+                 // Sets the page field to "/about.html".
+                 tracker.set('page', currentPath);
+                 tracker.send('pageview');
+             });*/
             /*if (!category) { category = 'Button' }
             ga('send', {
                 hitType: 'pageview',
@@ -33,20 +39,20 @@
                 location: location.href
             });*/
 
-            
+
         }
 
-       /* function sendClick(label, category) {
-            if (!category) { category = 'Button' }
-            ga('send', {
-                hitType: 'event',
-                eventCategory: category,
-                eventAction: 'click',
-                eventLabel: label,
-                page: location.hash.replace('#!', ''),
-                location: location.href
-            });
-        };*/
-};
+        /* function sendClick(label, category) {
+             if (!category) { category = 'Button' }
+             ga('send', {
+                 hitType: 'event',
+                 eventCategory: category,
+                 eventAction: 'click',
+                 eventLabel: label,
+                 page: location.hash.replace('#!', ''),
+                 location: location.href
+             });
+         };*/
+    };
 })();
 
--- config/versionFront.js
+++ config/versionFront.js
@@ -1,1 +1,1 @@
-var versionFront = '20170418.1029-20'; 
+var versionFront = '20170418.2035-24'; 
