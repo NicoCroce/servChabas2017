1068c091ff76f522b9d21387a248efa5cdf9952e
NicoCroce <ingcrocenicolas@outlook.com>
2017-04-26 18:30:42 -0300

    TEL - Se mejoró la sección de teléfonos

--- assets/app/app.modules.js
+++ assets/app/app.modules.js
@@ -7,11 +7,9 @@
             $rootScope.loadingService = true;
             $rootScope.showNavBar = window.location.hash.indexOf("home") < 0;
             $rootScope.modalIsOpen = false;
-            $rootScope.backSectionVisible = false;
-            /*$rootScope.backSectionVisible = false;*/
             analytics.pageview('/home');
             window.addEventListener("hashchange", function (event) {
-                $rootScope.showNavBar = window.location.hash.indexOf("home") < 0 || $rootScope.backSectionVisible;
+                $rootScope.showNavBar = angular.showNavBar();
             });
 
             if (typeof (Storage) !== "undefined" && !localStorage.getItem("showAddHomeModal")) {
--- assets/app/helper.js
+++ assets/app/helper.js
@@ -5,3 +5,23 @@ angular.isUndefinedOrNull = function(obj){
 angular.isUndefinedOrNullOrEmpty = function (obj) {
     return angular.isUndefined(obj) || obj === null ||  obj.length === 0 || typeof obj === 'object' && Object.keys(obj).length === 0;
 };
+
+angular.showNavBar = function() {
+    var noHome = window.location.hash.indexOf("home") < 0,
+        isServiceList = window.location.hash.indexOf("servicios/list") > 0,
+        isServiceType = window.location.hash.indexOf("servicios/") > 0;
+
+        if(!noHome && !isServiceType){
+            return false;
+        }
+
+        if(isServiceList){
+            return true;
+        }
+
+        if(isServiceType) {
+            return false;
+        }
+
+        return true;
+};
