d26823bb3233892ea6a4f1e80e697d4189ad2be9
NICOLÁS CROCE <NICOLS CROCE>
2017-06-07 00:22:44 -0300

    FIREBASE Se terminó servicios

--- assets/app/admin/servicios/ControllerServiciosAdmin.js
+++ assets/app/admin/servicios/ControllerServiciosAdmin.js
@@ -6,6 +6,11 @@
 
     function ControllerServiciosAdmin ($scope){
         $scope.isLoaded = false;
+        $scope.isLoading = true;
+        var typeService = '',
+            persistService = {},
+            localChanged = false;
+
         $scope.listOptions = {
             options: [],
             selected: 'Seleccione tipo de servicio',
@@ -19,20 +24,37 @@
             headers: {} 
         };
 
+        $scope.persistService;
+
         var usersDB = firebase.database().ref('data/servicios');
-        // Si se actualzia el valor se obtiene automáticamente. Como un OBSERVER 
+
         usersDB.on('value', function (data) {
+            console.log("entraaaaaaaaa");
             $scope.listOptions.options = Object.keys(data.val());
             $scope.allServicies = data.val();
-            $scope.service.data = data.val()["remises"];
+            $scope.service.data = data.val()[typeService];
             $scope.isLoaded = true;
-            /*$scope.allUtils = data.val();
-            debugger;*/
+            persistService = data.val();
+            $scope.isLoading = false;
+            if (localChanged) { // evita apply si los cambios son locales. 
+                localChanged = false;
+                return;
+            }
             $scope.$apply();
         });
 
+        $scope.setValue = function() {
+            localChanged = true;
+            firebase.database().ref('data/servicios/' + typeService).update($scope.service.data);
+        }
+
+        $scope.isObject = function(element) {
+            return typeof element == "object";
+        }
+
         $scope.$watch('listOptions.selected', function(val){
             $scope.service.data = $scope.allServicies[val];
+            typeService = val;          
         }, true);
     };
 })();
