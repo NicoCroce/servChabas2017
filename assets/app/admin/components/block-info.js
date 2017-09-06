e2f7039dbdbd3bfb5cf3c06858d91d3b19b4ff99
Nicolas Croce <nicolas.croce@bbva.com>
2017-09-06 20:18:11 -0300

    ADMIN - Se agregaron los iconos de borrar y agregar. TODO: Agregar fixed y dar acción a eliminar elemento.

--- assets/app/admin/components/block-info.js
+++ assets/app/admin/components/block-info.js
@@ -45,15 +45,16 @@
 
                     var templateDown = '<button ng-click="downLevel(\'' + index + '\')" class="icon-arrow_right bt-down"></button>',
                         templateUp = '<button ng-click="upLevel(\'' + index + '\')" class="icon-arrow_right bt-up"></button>',
+                        templateDelete = '<button ng-click="deleteElement(\'' + index + '\')" class="icon-delete"></button>',
                         templateControl = '';
 
                     if (!scope.addElement) {
                         if (index == firstKey) {
-                            templateControl = templateDown;
+                            templateControl = templateDelete + templateDown;
                         } else if (index == lastKey) {
-                            templateControl = templateUp;
+                            templateControl = templateDelete + templateUp;
                         } else {
-                            templateControl = templateUp + templateDown;
+                            templateControl = templateDelete + templateUp + templateDown;
                         }
                         templateControl = '<div class="control-block">' + templateControl + '</div>';
                     } else {
